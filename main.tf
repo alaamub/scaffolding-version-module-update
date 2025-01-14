07bec5323da858a59ca1ed09ecd3b4a30479ab11
Aladdin Almubayed <522779+alaamub@users.noreply.github.com>
2025-01-14 13:07:23 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 # main.tf
 
-resource "null_resource" "foo" {
+resource "null_resource" "foo2" {
   triggers = {
     foo = var.project
   }
+}
+
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.4.0"
 }
