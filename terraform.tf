850fccfd115eac47d02402b98e9c8a9234263c43
Aladdin Almubayed <522779+alaamub@users.noreply.github.com>
2025-01-14 14:54:05 -0800

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "5.34.0"
     }
 
     github = {
       source  = "integrations/github"
+      version = "5.45.0"
     }
 
     google = {
       source  = "hashicorp/google"
+      version = "5.14.0"
     }
 
     null = {
