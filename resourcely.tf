6efcdf3db5e5164319863ca94a4a5e0abdffdbc4
Aladdin Almubayed <522779+alaamub@users.noreply.github.com>
2025-02-13 11:15:53 -0800

    Update resourcely.tf

--- resourcely.tf
+++ resourcely.tf
@@ -1,5 +1,6 @@
 resource "terraform_data" "resourcely_metadata" {
   input = {
+     // this is not module version. this is resourcely schema version.
      version = 1
      modules = file(".terraform/modules/modules.json")
   }
